399fb92de62606c7fc95e445e677c4b24fecbf45
anhvv <vuvietanh02042004@gmail.com>
2024-10-10 17:06:08 +0700

    commit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // dd($request->headers->all());
+        // return $request->expectsJson() ? null : route('login');
+        return "";
     }
 
     
